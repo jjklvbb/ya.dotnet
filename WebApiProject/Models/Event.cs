9dd203aaa390c3a4344a4de3c869474e8fab252b
Гладких Екатерина <gladkih@it-limon.ru>
2026-07-10 18:33:44 +0500

    Создала модель, интерфейс и сервис

---
+++ WebApiProject/Models/Event.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApiProject.Models
+{
+    public class Event
+    {
+        public Guid Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string? Description { get; set; }
+
+        public DateTime StartAt { get; set; }
+
+        public DateTime EndAt { get; set; }
+    }
+}
---
+++ WebApiProject/Services/EventService.cs
@@ -0,0 +1,41 @@
+﻿using WebApiProject.Interfaces;
+using WebApiProject.Models;
+
+namespace WebApiProject.Services
+{
+    public class EventService : IEventService
+    {
+        // можно было использовать Dictionary
+        public static List<Event> events {  get; private set; }
+
+        public List<Event> GetAllEvents()
+        {
+            return events;
+        }
+
+        public Event? GetEventById(Guid id)
+        {
+            return events.Where(e => e.Id == id).FirstOrDefault();
+        }
+
+        public void CreateEvent(Event newEvent)
+        {
+            events.Add(newEvent);
+        }
+
+        public void UpdateEvent(Guid id, Event newEvent)
+        {
+            Event? ev = events.Find(e => e.Id == id);
+
+            if (ev == null)
+                throw new Exception("Не найдено событие для обновления");
+
+            ev = newEvent;
+        }
+
+        public void DeleteEvent(Guid id)
+        {
+            events.RemoveAll(e => e.Id == id);
+        }
+    }
+}
